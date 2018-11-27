fbc2c7cbd8527c90418b5e3f29d7fbc6ea5fc4e9
trevaun.miller <trevaun.miller@aye-tech.com>
2018-11-27 11:04:38 -0500

    Removed hardcoded product categories from the index(main) page. Added for each loop to populate this page with products.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+productA = {name:"Patty",symbol:"$", currency:"JMD", cost:"432", imageURL:'img/bg-img/10.jpg'};
+productB = {name:"Patty",symbol:"$", currency:"JMD", cost:"432", imageURL:'img/bg-img/10.jpg'};
+var products = [];
+products.push(productA);
+products.push(productB);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Loc8' });
+  res.render('index', { title: 'Loc8', products: products });
 });
 
 module.exports = router;
